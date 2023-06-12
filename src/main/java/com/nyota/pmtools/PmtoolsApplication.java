841d18f9d35a9c8bfdaff3e76f739713bf4e5a90
Mukul Kumar <kumarmuk@gmail.com>
2023-06-12 11:16:50 +0100

    first commit

---
+++ src/main/java/com/nyota/pmtools/PmtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.nyota.pmtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyota/pmtools/PmtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyota.pmtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
